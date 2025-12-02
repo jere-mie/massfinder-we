2baa09016e75bc163a252f8c5724416ec111199f
justinbornais <justin.bornais@gmail.com>
2025-12-02 11:45:08 -0500

    Added to mobile menu as well

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -116,6 +116,12 @@ export function Navbar() {
           >
             Mass, Confession, & Adoration Times
           </a>
+          <a
+            href="/events"
+            className="block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-gray-900 hover:bg-gray-50"
+          >
+            Local Events
+          </a>
         </div>
       </div>
     </nav>
