1d96416252034ca686417fbfff934c551f2da33e
justinbornais <justin.bornais@gmail.com>
2025-12-02 11:36:19 -0500

    Added link to Events page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,6 +38,12 @@ export function Navbar() {
             >
               Mass, Confession, & Adoration Times
             </a>
+            <a
+              href="/events"
+              className="text-gray-600 hover:text-gray-900 px-3 py-2 text-sm font-medium transition-colors"
+            >
+              Local Events
+            </a>
           </div>
 
           {/* Mobile menu button */}
